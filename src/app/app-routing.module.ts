e79fa04cb285eed6f9b1777317ef99d8ede32ba2
flung34 <flung34@gmail.com>
2020-02-04 19:09:30 +0100

    fran

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
     path: 'alumnos',
     loadChildren: () => import('./Screens/alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
   {
     path: 'empresas',
     loadChildren: () => import('./Screens/empresas/empresas.module').then( m => m.EmpresasPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Screens/login/login.module').then( m => m.LoginPageModule)
   },
 
 ];
