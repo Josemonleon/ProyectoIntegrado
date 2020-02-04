6ea3c5774117fb306e3cb30919b66fa13f5a39d9
Jose Monleon <jmonleonlopez@gmail.com>
2020-02-04 15:38:13 +0100

    Añadir funcionalidad para comprobar datos al añadir empresas

--- src/app/Screens/add-empresa/add-empresa.page.ts
+++ src/app/Screens/add-empresa/add-empresa.page.ts
@@ -45,12 +45,42 @@ export class AddEmpresaPage implements OnInit {
                                     "Contacto": this.contacto,                             
                                   };
 
-
+  if(this.esCorrecto()){
     //Llamamos a la función que añade empresa en la bbdd de FireBase. Le pasamos el empresa por parametro.
     this._service.setEmpresa(empresa);
 
 
     this.presentToast();  //Se llama para que muestre el mensaje de que ha sido insertado un producto.
+  }else alert("Datos incorrectos");
+    
+
+  }
+
+
+  //Funcion para comprobar si los datos introducidos son correctos
+  esCorrecto(){
+
+    let  nombreOK, localidadOK, contactoOK;
+
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var regex2 = /^[a-zA-Z ]{2,}$/;
+
+    if (regex.test(this.contacto)) {
+      contactoOK = true;
+    } else {
+      contactoOK = false;
+    }
+
+    (regex2.test(this.nombre)) ? nombreOK=true : nombreOK=false;
+    (regex2.test(this.localidad)) ? localidadOK=true : localidadOK=false;
+
+    if(nombreOK && localidadOK && contactoOK){
+      console.log("Todo correcto");
+      return true;
+    } else {
+      console.log("Algun error");
+      return false;
+    }
 
   }
 
