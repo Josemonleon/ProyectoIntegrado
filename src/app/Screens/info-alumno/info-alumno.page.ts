1a2d4dfc6517bba156d2a368abc602f644c505de
Jose Monleon <jmonleonlopez@gmail.com>
2020-02-10 17:22:58 +0100

    Añadir Dni a información de alumno

--- src/app/Screens/info-alumno/info-alumno.page.ts
+++ src/app/Screens/info-alumno/info-alumno.page.ts
@@ -43,7 +43,8 @@ export class InfoAlumnoPage implements OnInit {
     "Localidad": "",
     "Nombre": "",
     "Tutor": "",
-    "Empresa": ""
+    "Empresa": "",
+    "Dni": ""
   };
 
   empresa: Object = {}
